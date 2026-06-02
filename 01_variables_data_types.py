a1fc6b666be4e6da4332144aedaea5593190920e
Joshua Flora <joshuaflora1016@gmail.com>
2026-06-02 11:02:54 +0800

    Initial commit: Set up fundamentals and add variables lesson

---
+++ 01_variables_data_types.py
@@ -0,0 +1,19 @@
+# Python Basics: Variables and Data Types
+
+#1. String Data Type
+user_name = "Joshua"
+
+#2. Interger Data Type 
+user_age = 30
+
+#3. Float Data Type
+user_height = 5.9
+
+#4. Boolean Data Type
+is_learning_python = True
+
+# Printing output using f-strings
+print(f"User Name: {user_name}")
+print(f"User Age: {user_age}")
+print(f"User Height: {user_height}")
+print(f"Is Learning Python: {is_learning_python}")
